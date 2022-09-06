b6ee41ca2c85c38f73485707680bd11c39834bf8
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-06 05:12:07 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
-
+  before_action :ensure_correct_user, only: [:update, :edit]
+  
   def show
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
     @book_new = Book.new
   end
 
@@ -35,9 +35,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -45,5 +45,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
